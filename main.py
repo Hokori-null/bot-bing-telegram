c2bcee8df814d402ec73cee481d569f17a02ef17
Yakumo Hokori <yakumoy847@gmail.com>
2023-06-25 17:46:47 +0800

    添加日志

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import re
 import config
+import os
+import logging
 from module import chat_bot
 
 from telebot.async_telebot import AsyncTeleBot
@@ -62,13 +64,21 @@ async def ask(message, is_ref=False):
         #     return
         namez = username
         prompt_text = message.text
+        log_dir = "./log"
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        log_file = os.path.join(log_dir, f"{username}.log")
+        logging.basicConfig(filename=log_file, level=logging.INFO) 
+        logging.info(f"Request received from {username} - {prompt_text}")
         print(f"Request received from {username} - {prompt_text}")
         if not prompt_text:
             await bot.reply_to(message, "Empty query sent. Add your query /ask <message>")
         else:
             bot_response = await bingChat(prompt_text,namez, is_ref)
             print(f"Response received - {bot_response}")
+            qre = bot_response.replace('?\n\n', '')
             await bot.reply_to(message, bot_response.replace('?\n\n', ''))
+            logging.info(qre)
     except Exception as e:
         print("Exception happened qa")
         print(e)
